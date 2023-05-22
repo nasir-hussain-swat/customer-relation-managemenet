7a1ce6b5c9be17a0aca8ab6e11c56fbef8f2024d
nasirhusain <nasir.swat.hussain@gmail.com>
2023-05-22 03:31:28 -0700

    adding little functionality

--- website/views.py
+++ website/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate,login,logout
+from django.contrib import messages
+
 
 # Create your views here.
+def home(request):
+    #check to see if logging in
+    if request.method =='POST':
+        username=request.POST['username']
+        password=request.POST['password']
+
+        #authenticate
+        user=authenticate(request,username=username,password=password)
+        if user is not None:
+            login(request,user)
+            messages.success(request,"you have been logged in successfully")
+            return redirect('home')
+        else:
+            messages.success(request,'there was an error plz try again')
+            return redirect('home')
+
+    else:
+        return render(request,'home.html',{})
+
+
+
+
+
+def logout_user(request):
+    pass
