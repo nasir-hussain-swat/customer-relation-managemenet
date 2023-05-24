1ca97b6e69f3c75d717380726eb55a10811f58a3
nasirhusain <nasir.swat.hussain@gmail.com>
2023-05-24 11:42:05 -0700

    registration commit

--- website/urls.py
+++ website/urls.py
@@ -5,7 +5,9 @@ urlpatterns = [
     path('',views.home,name='home'),
     #     path('login/',views.login_user,name='login'),
 
-    # path('logout',views.logout_user,name='logout'),
+    path('logout',views.logout_user,name='logout'),
+        path('register',views.register_user,name='register'),
+
 
     
 ]
--- website/views.py
+++ website/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate,login,logout
 from django.contrib import messages
+from .forms import SignUpForm
 
 
 # Create your views here.
 def home(request):
     #check to see if logging in
-    if request.method =='POST':
+    if request.method == 'POST':
         username=request.POST['username']
         password=request.POST['password']
 
@@ -28,4 +29,29 @@ def home(request):
 
 
 def logout_user(request):
-    pass
+    logout(request)
+    messages.success(request,'you have logout')
+    return redirect('home')
+
+
+def register_user(request):
+    if request.method == 'POST':
+        form=SignUpForm(request.POST)
+        
+        if form.is_valid():
+            form.save()
+            #authenticate and login
+            username=form.cleaned_data['username']
+            password=form.cleaned_data['password1']
+
+            user=authenticate(username=username,password=password)
+            login(request,user)
+            messages.success(request,'Registered ')
+            return redirect('home')
+        
+    else:
+        
+        form=SignUpForm()
+        return render(request,'register.html',{'form':form})
+    
+    return render(request,'register.html',{'form':form})
