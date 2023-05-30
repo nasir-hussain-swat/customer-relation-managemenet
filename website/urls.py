9ca2dd9e4d4fc03d9610ed705baf6ad9d9f18adf
nasirhusain <nasir.swat.hussain@gmail.com>
2023-05-30 12:03:11 -0700

    project completed

--- website/urls.py
+++ website/urls.py
@@ -1,6 +1,6 @@
 
 from django.urls import path
-from . import views
+from .import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('logout', views.logout_user, name='logout'),
@@ -10,6 +10,7 @@ urlpatterns = [
     
     path('delete_record/<int:pk>', views.delete_record, name='delete_record'),
     path('add_record/', views.add_record, name='add_record'),
+    path('update_record/<int:pk>', views.update_record, name='update_record'),
 
 
 
--- website/views.py
+++ website/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate,login,logout
 from django.contrib import messages
-from .forms import SignUpForm
+from .forms import SignUpForm,AddRecordForm
 from .models import Record
 
 
@@ -88,5 +88,32 @@ def delete_record(request,pk):
         return redirect('home')
     
 def add_record(request):
-    return render(request,'add_record.html',{})
+    form=AddRecordForm(request.POST or None)
+    if request.user.is_authenticated:
+        if request.method == 'POST':
+            if form.is_valid():
+                add_record=form.save()
+                messages.success(request,'Record added')
+                return redirect('home')
+            
+        return render(request,'add_record.html',{'form':form})
+    else:
+        messages.success(request,'you must login to add record')
+        return redirect('home')
+
+def update_record(request,pk):
+    if request.user.is_authenticated:
+        current_record=Record.objects.get(id=pk)
+        form=AddRecordForm(request.POST or None,instance=current_record)
+        if form.is_valid():
+            form.save()
+            messages.success(request,'updated record')
+            return redirect('home')
+        return render(request,'update_record.html',{'form':form})
+    else:
+        messages.success(request,'you must login to add record')
+        return redirect('home')
+
+
+        
 
